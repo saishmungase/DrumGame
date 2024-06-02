9ad9443cacc412ee53305b1c6c4cc313b86afb65
saishmungase <147045067+saishmungase@users.noreply.github.com>
2024-06-02 20:27:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+for(var a = 0; a<document.querySelectorAll(".drum").length; a++){
+    document.querySelectorAll(".drum")[a].addEventListener("click", function(){
+        var val = this.innerHTML;
+        
+        play(val);
+
+
+    })
+}
+
+document.addEventListener("keypress", function(input){
+    play(input.key)
+})
+
+
+function play(clicked){
+    switch (clicked) {
+        case "w":
+                var tom1 = new Audio('./sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            
+            case "a": 
+                var tom2 = new Audio('./sounds/tom-2.mp3');
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio('./sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio('./sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+            case "j":
+                var kick = new Audio('./sounds/kick-bass.mp3');
+                kick.play();
+                break;
+
+            case "k":
+                var snare = new Audio ('./sounds/snare.mp3');
+                snare.play();
+                break;
+
+            case "l":
+                var crash = new Audio('./sounds/crash.mp3');
+                crash.play();
+                break;
+
+            default:
+                alert("plzz press only given button.")
+        }
+    }
